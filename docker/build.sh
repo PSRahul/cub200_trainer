596670cd0f5f2bb2b8e54977d0dce019fed3eb1d
PSRahul <rahulpsr95@gmail.com>
2022-07-12 18:46:51 +0200

    added the server configs

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-podman build -t rsr/trainer .
+podman pull docker.io/pytorch/pytorch:1.12.0-cuda11.3-cudnn8-runtime
+
--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,7 @@
-podman run --rm --gpus device=0 -it \
--v "/mnt/data/home/rsr/MasterThesis/:/home/psrahul/MasterThesis/" \
---name rsr_trainer \
---shm-size=2gb \
-localhost/rsr/trainer :latest
+podman run -v "/mnt/data/home/rsr/MasterThesis/:/home/psrahul/MasterThesis/"  --net=host --ipc=host --gpus all -ti --name rsr_trainer docker.io/pytorch/pytorch:1.12.0-cuda11.3-cudnn8-runtime   
+
+
+
+apt-get update && apt-get install -y python3 python3-dev python3-pip  build-essential cmake \
+    && pip3 install --no-cache-dir --upgrade pip setuptools
+
