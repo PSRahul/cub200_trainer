4b54a4130c6d85a023826c41ec51a38dff2172ce
PSRahul <rahulpsr95@gmail.com>
2022-07-12 10:38:00 +0200

    add transform and launch files

--- data/classification/data_module.py
+++ data/classification/data_module.py
@@ -1,3 +1,4 @@
+from cgi import test
 import pytorch_lightning as pl
 from torch.utils.data import random_split, DataLoader
 from torchvision.datasets import ImageFolder
@@ -7,18 +8,28 @@ from typing import Optional
 import os
 
 class ClassificationDataModule(pl.LightningDataModule):
-    def __init__(self, config):
+
+    def __init__(self, config,train_transforms,test_transforms):
         super().__init__()
         self.cfg = config
-        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-        self.prepare_data()
+        self.train_transforms=train_transforms
+        self.test_transforms=test_transforms
+        self.prepare_data(train_transforms,test_transforms)
 
-    def prepare_data(self):
+    def prepare_data(self,train_transforms,test_transforms):
         
-        train_dataset=ImageFolder(root=os.path.join(self.cfg["data"]["root_folder"],
-                                                        self.cfg["data"]["train_folder"]))
-        val_dataset=ImageFolder(root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["val_folder"]))
-        test_dataset=ImageFolder(root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["test_folder"]))
+        train_dataset=ImageFolder(
+            root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["train_folder"]),
+            transform=train_transforms
+            )
+
+        val_dataset=ImageFolder(
+            root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["val_folder"]), 
+            transform=test_transforms)
+
+        test_dataset=ImageFolder(
+            root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["test_folder"]), 
+            transform=test_transforms)
 
         print("Datasets are Accessible")            
     
@@ -26,12 +37,22 @@ class ClassificationDataModule(pl.LightningDataModule):
 
         # Assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
-            self.train_dataset=ImageFolder(root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["train_folder"]))
-            self.val_dataset=ImageFolder(root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["val_folder"]))
+            self.train_dataset=ImageFolder(
+            root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["train_folder"]),
+            transform=self.train_transforms
+            )
+
+        val_dataset=ImageFolder(
+            self.root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["val_folder"]), 
+            transform=self.test_transforms
+            )
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
-            self.test_dataset = ImageFolder(root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["test_folder"]))
+            self.test_dataset=ImageFolder(
+            root=os.path.join(self.cfg["data"]["root_folder"],self.cfg["data"]["test_folder"]), 
+            transform=self.test_transforms
+            )
 
         
     def train_dataloader(self):
--- main.py
+++ main.py
@@ -4,18 +4,24 @@ from data.classification.data_module import ClassificationDataModule
 from yaml.loader import SafeLoader
 from network.model import ResNet18Model
 
+
 def load_config():
-    with open("config.yaml","r") as f:
-            config = yaml.load(f, Loader=SafeLoader)
-            
+    with open("config.yaml", "r") as f:
+        config = yaml.load(f, Loader=SafeLoader)
+
     return config
 
+
 def main():
-    cfg=load_config()
-    ClassificationDataModule(cfg)
-    model=ResNet18Model(cfg)
-    print(model.print_details())
-    #print(model.get_test_transforms())   
+    cfg = load_config()
+
+    model = ResNet18Model(cfg)
+    data = ClassificationDataModule(
+        cfg,
+        train_transforms=model.get_train_transforms(),
+        test_transforms=model.get_test_transforms(),
+    )
+
 
 if __name__ == "__main__":
     sys.exit(main())
