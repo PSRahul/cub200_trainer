a9564a345e8af4a590bb689fa9e63ef81377e228
PSRahul <rahulpsr95@gmail.com>
2022-07-18 11:33:16 +0200

    fix ViT learning rate to 0.0002291

--- network/network_module.py
+++ network/network_module.py
@@ -59,8 +59,9 @@ class ClassificationModel(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
-            self.model.parameters(), lr=self.hparams.lr
-        )  # lr=1e-5)
+            self.model.parameters(), lr=0.0002291)
+            #lr=self.hparams.lr
+         # lr=1e-5)
         return optimizer
 
 
